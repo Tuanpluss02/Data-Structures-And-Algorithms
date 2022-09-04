47f3b29c417d0ed3ff2b386c9a584b009957c278
Tuan Plus <Dongoctuan2002kk@gmail.com>
2022-09-05 01:48:56 +0700

    update main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,53 +1,53 @@
-#include "singlyLinkedList.h"
-#include "doublyLinkedList.h"
+// #include "singlyLinkedList.h"
+// #include "doublyLinkedList.h"
 #include "circularLinkedList.h"
 #include <iostream>
 using namespace std;
 int main()
 {
     // test singly linked list
-    cout << "Singly Linked List:" << endl;
-    singlyLinkedList list1;
-    list1.add(3);
-    list1.add(4);
-    list1.add(5);
-    list1.add(1);
-    list1.add(2);
-    list1.print();
-    list1.remove(3);
-    list1.print();
-    list1.remove(1);
-    list1.print();
-    list1.add(200);
-    list1.add(100, 2);
-    list1.print();
-    list1.sort();
-    list1.print();
-    cout << "The length of the list is " << list1.size() << endl;
+    // cout << "Singly Linked List:" << endl;
+    // singlyLinkedList list1;
+    // list1.add(3);
+    // list1.add(4);
+    // list1.add(5);
+    // list1.add(1);
+    // list1.add(2);
+    // list1.print();
+    // list1.remove(3);
+    // list1.print();
+    // list1.remove(1);
+    // list1.print();
+    // list1.add(200);
+    // list1.add(100, 2);
+    // list1.print();
+    // list1.sort();
+    // list1.print();
+    // cout << "The length of the list is " << list1.size() << endl;
 
-    // test doubly linked list
-    cout << "Doubly Linked List:" << endl;
-    doublyLinkedList list2;
-    list2.add(3);
-    list2.add(4);
-    list2.add(5);
-    list2.add(1);
-    list2.add(2);
-    list2.printForward();
-    list2.remove(3);
-    list2.printForward();
-    list2.remove(1);
-    list2.printForward();
-    list2.add(200);
-    list2.add(100, 2);
-    list2.add(-20, 4);
-    list2.printForward();
-    list2.sortAscending();
-    list2.printForward();
-    list2.sortDescending();
-    list2.printForward();
-    list2.printBackward();
-    cout << "The length of the list is " << list2.size() << endl;
+    // // test doubly linked list
+    // cout << "Doubly Linked List:" << endl;
+    // doublyLinkedList list2;
+    // list2.add(3);
+    // list2.add(4);
+    // list2.add(5);
+    // list2.add(1);
+    // list2.add(2);
+    // list2.printForward();
+    // list2.remove(3);
+    // list2.printForward();
+    // list2.remove(1);
+    // list2.printForward();
+    // list2.add(200);
+    // list2.add(100, 2);
+    // list2.add(-20, 4);
+    // list2.printForward();
+    // list2.sortAscending();
+    // list2.printForward();
+    // list2.sortDescending();
+    // list2.printForward();
+    // list2.printBackward();
+    // cout << "The length of the list is " << list2.size() << endl;
 
     // test circular linked list
     cout << "Circular Linked List:" << endl;
