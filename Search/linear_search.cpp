d294748f5e281682907a97c6d77f7e4250d7588b
Tuân Đỗ <82562559+Tuanpluss02@users.noreply.github.com>
2024-02-05 05:49:47 +0000

    (update) linear search

--- Search/linear_search.cpp
+++ Search/linear_search.cpp
@@ -3,15 +3,12 @@
 template <typename T>
 long SearchAlgorithm<T>::linear_search(vector<T> &arr, T target)
 {
-    unordered_map<T, long> hashmap;
     for (long i = 0; i < arr.size(); i++)
     {
-        hashmap[arr[i]] = i;
-    }
-    auto it = hashmap.find(target);
-    if (it != hashmap.end())
-    {
-        return it->second;
+        if (arr[i] == target)
+        {
+            return i;
+        }
     }
     return -1;
 }
