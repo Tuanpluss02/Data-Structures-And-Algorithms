8ed8e66f06d7139297579812e01e851d26151e66
Tuân Đỗ <82562559+Tuanpluss02@users.noreply.github.com>
2024-02-05 03:47:22 +0000

    (feat) create Search Algorithm lib

--- Search/hashmap_search.cpp
+++ Search/hashmap_search.cpp
@@ -1,29 +1,7 @@
-#include <bits/stdc++.h>
-
-#define faster()                  \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
-#define clean() cin.ignore(numeric_limits<streamsize>::max(), '\n');
-#define pb push_back
-#define fi first
-#define se second
-#define Mod 1000000007
-#define ld long double
-#define ll long long
-#define lli unsigned long long int
-#define For(i, a, b) for (ll i = a; i < b; ++i)
-#define Forr(i, a, b) for (ll i = a; i >= b; --i)
-#define vec vector<ll>
-#define sortu(c) sort(c.begin(), c.end())
-#define sortd(c) sort(c.rbegin(), c.rend())
-#define rev(c) reverse(c.begin(), c.end())
-#define pause() system("pause");
-
-using namespace std;
+#include "SearchAlgorithm.hpp"
 
 template <typename T>
-long hashmap_search(vector<T> arr, T target)
+long SearchAlgorithm<T>::hashmap_search(vector<T> &arr, T target)
 {
     unordered_map<T, long> hashmap;
     for (long i = 0; i < arr.size(); i++)
@@ -35,15 +13,5 @@ long hashmap_search(vector<T> arr, T target)
     {
         return it->second;
     }
-
     return -1;
-}
-
-int main()
-{
-    faster();
-    vector<int> arr = {1, 2, 3, 4, 5};
-    int target = 3;
-    cout << hashmap_search(arr, target);
-    return 0;
 }
--- Search/ternary_search.cpp
+++ Search/ternary_search.cpp
@@ -1,29 +1,7 @@
-#include <bits/stdc++.h>
-
-#define faster()                  \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
-#define clean() cin.ignore(numeric_limits<streamsize>::max(), '\n');
-#define pb push_back
-#define fi first
-#define se second
-#define Mod 1000000007
-#define ld long double
-#define ll long long
-#define lli unsigned long long int
-#define For(i, a, b) for (ll i = a; i < b; ++i)
-#define Forr(i, a, b) for (ll i = a; i >= b; --i)
-#define vec vector<ll>
-#define sortu(c) sort(c.begin(), c.end())
-#define sortd(c) sort(c.rbegin(), c.rend())
-#define rev(c) reverse(c.begin(), c.end())
-#define pause() system("pause");
-
-using namespace std;
+#include "SearchAlgorithm.hpp"
 
 template <typename T>
-long ternary_search_recursion(vector<T> arr, T target, long start, long end)
+long SearchAlgorithm<T>::ternary_search_recursion(vector<T> &arr, T target, long start, long end)
 {
     long size = arr.size();
     if (arr.empty())
@@ -49,7 +27,7 @@ long ternary_search_recursion(vector<T> arr, T target, long start, long end)
 }
 
 template <typename T>
-long ternary_search_no_recursion(vector<T> arr, T target, long start, long end)
+long SearchAlgorithm<T>::ternary_search_no_recursion(vector<T> &arr, T target, long start, long end)
 {
     if (arr.empty())
         throw invalid_argument("Vector is empty!");
@@ -75,13 +53,4 @@ long ternary_search_no_recursion(vector<T> arr, T target, long start, long end)
         }
     }
     return -1;
-}
-
-int main()
-{
-    faster();
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
-    cout << ternary_search_no_recursion(arr, 5, 0, arr.size() - 1) << endl;
-    cout << ternary_search_recursion(arr, -1, 0, arr.size() - 1) << endl;
-    return 0;
 }
